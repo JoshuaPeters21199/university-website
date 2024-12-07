634cc80e368e85a000b04481ce160fc63454fb68
JoshuaPeters <jgp2.11.99@gmail.com>
2024-12-07 08:03:13 -0700

    Initialized project.

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            async authorize(credentials) {
+                try {
+                    await connectDB();
+                    const user = await User.findOne({ username: credentials.username })
+
+                    if (!user) {
+                        console.log('Invalid username or password');
+                        return null;
+                    }
+
+                    const isValidPassword = await bcrypt.compare(credentials.password, user.password);
+                    if (!isValidPassword) {
+                        console.log('Invalid username or password');
+                        return null;
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        name: `${user.firstName} ${user.lastName}`,
+                        role: user.role,
+                        school: user.school,
+                    }
+                } catch (error) {
+                    console.log('An error occurred: ', error)
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            session.user.role = token.role;
+            session.user.school = token.school;
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.role = user.role;
+                token.school = user.school;
+            }
+            return token;
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler  = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ models/user.js
@@ -0,0 +1,13 @@
+import mongoose, { models, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    school: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    role: { type: String, enum: ['Admin', 'Teacher', 'TA', 'Student'], required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+})
+
+const User = models.User || mongoose.model('User', userSchema);
+export default User;
